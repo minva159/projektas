f174d1912053049377239f70055c56da5e7ef023
minva159 <105968838+minva159@users.noreply.github.com>
2022-10-27 17:22:38 +0300

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://vanami:vanami12@cluster0.qvpyip3.mongodb.net/mongo', {useNewUrlParser: true, useUnifiedTopology:true},
-err => {
-    if (!err)
-    console.log('Mongodb connection succeeded.')
-    else
-    console.log('Error while connecting Mongodb : '+ JSON.stringify(err, undefined, 2))
-});
